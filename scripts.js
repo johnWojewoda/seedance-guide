b09e9f466d1fa1482af63624a6fba3fb04ad0633
John Wojewoda <wojewodajohn@gmail.com>
2026-05-03 20:51:56 -0400

    Fix scripts.js corruption

--- scripts.js
+++ scripts.js
@@ -34,8 +34,3 @@ async function pollPrediction(predictionId, statusEl) {
         await new Promise(resolve => setTimeout(resolve, 3000));
     }
 }
-
-
-git add .
-git commit -m "Shorten API key section for section 2"
-git push
